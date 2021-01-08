50dddb135964e9c402b24e6e1c33ae76ce28125c
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-01-08 11:05:23 +0100

    PHP-ORDER: Adding a message when there is invalid input in the form
    Saving the previous value when having a from error

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+// This file is your starting point (= since it's the index)
+// It will contain most of the logic, to prevent making a messy mix in the html
+
+// This line makes PHP behave in a more strict way
+declare(strict_types=1);
+
+// We are going to use session variables so we need to enable sessions
+session_start();
+
+// Use this function when you need to need an overview of these variables
+function whatIsHappening() {
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+
+// TODO: provide some products (you may overwrite the example)
+$products = [
+    ['name' => 'Kitty Catnip', 'price' => 3.75],
+    ['name' => 'Doggo Bone', 'price' => 6.00],
+    ['name' => 'Rolling Stone', 'price' => 12.30],
+    ['name' => 'Flappy bored', 'price' => 7.90],
+    ['name' => 'It\'s-a-rock', 'price' => 33.60],
+];
+
+$totalValue = 0;
+
+require 'order.php';
---
+++ order.php
@@ -0,0 +1,218 @@
+<?php // This files is mostly containing things for your view / html ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"
+          rel="stylesheet"/>
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Yellowtail&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=East+Sea+Dokdo&display=swap" rel="stylesheet">
+
+    <title>Order now!</title>
+</head>
+<body>
+
+    <header>
+        <img class="company-logo" src="assets/catPlay.png" alt="">
+        <h1>Animal King <img class="title-icon" src="assets/paws.png" alt=""></h1>
+    </header>
+    <br>
+    <hr>
+    <div class="navbar">
+        <p>Tigers</p>
+        <p>Wolfs</p>
+        <p>Capybara's</p>
+        <p>Condors</p>
+        <p>Sharks</p>
+    </div>
+
+    <div class="container">
+        <h1>Place your order</h1>
+        <?php // Navigation for when you need it ?>
+        <?php 
+            pre_r($_POST);
+
+            function test_input($data) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+            }
+
+            if (empty($_POST["email"])) {
+                $emailErr = "Email is required";
+                echo '<div class="alert alert-danger" role="alert">
+                <h4 class="alert-heading">Look Again!</h4>
+                <p>Aww... It seems you don\'t love your pets! please fill in all the required info!</p>
+                <hr>
+                <p class="mb-0">Be quick about it!!.</p>
+                </div>';
+            } else {
+                $email = test_input($_POST["email"]);
+                // check if e-mail address is well-formed
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    $emailErr = "Invalid email format";
+
+                    echo '<div class="alert alert-danger" role="alert">
+                    <h4 class="alert-heading">Look Again!</h4>
+                    <p>Aww... It seems you don\'t love your pets! please fill in all the required info!</p>
+                    <hr>
+                    <p class="mb-0">Be quick about it!!.</p>
+                    </div>';
+                }
+            }
+            
+            
+            echo $products['name'];
+
+            
+            
+        ?>
+
+        <form method="post">
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="email">E-mail:</label>
+                    <input type="text" id="email" name="email" class="form-control" value="<?php echo isset($_POST['email']) ? $_POST['email'] : '' ?>" required/>
+                    <span class="error">* <?php echo $emailErr;?></span>
+                </div>
+                <div></div>
+            </div>
+
+            <fieldset>
+                <legend>Address</legend>
+
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="street">Street:</label>
+                        <input type="text" name="street" id="street" class="form-control" value="<?php echo isset($_POST['street']) ? $_POST['street'] : '' ?>" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="streetnumber">Street number:</label>
+                        <input type="text" id="streetnumber" name="streetnumber" class="form-control" value="<?php echo isset($_POST['streetnumber']) ? $_POST['streetnumber'] : '' ?>" required>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="city">City:</label>
+                        <input type="text" id="city" name="city" class="form-control" value="<?php echo isset($_POST['city']) ? $_POST['city'] : '' ?>" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="zipcode">Zipcode</label>
+                        <input type="number" id="zipcode" name="zipcode" class="form-control" value="<?php echo isset($_POST['zipcode']) ? $_POST['zipcode'] : '' ?>" required>
+                    </div>
+                </div>
+            </fieldset>
+
+            <fieldset>
+                <legend>Products</legend>
+
+                <div class="container">
+                <div class="row">
+                <div class="col mt-40 mb-30">
+
+                    <div class="product-box cat-toys">
+                        <div class="card" style="width: 18rem;">
+                            <img src="assets/catnipToy.jfif" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title" name="Kitty Catnip">Kitty Catnip</h5>
+                                <p class="card-text">Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff.</p>
+                                <p>€ <span>3.75</span></p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col mt-40 mb-30">
+                    <div class="product-box dog-toys">
+                        <div class="card" style="width: 18rem;">
+                            <img src="assets/Doggo Bone.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Doggo Bone</h5>
+                                <p class="card-text">Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff.</p>
+                                <p>€ <span>6.00</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mt-40 mb-30">
+                    <div class="product-box rodent-toys">
+                        <div class="card" style="width: 18rem; ">
+                            <img src="assets/hamster ball.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Rolling Stone</h5>
+                                <p class="card-text">Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff.</p>
+                                <p>€ <span>12.30</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col mt-40 mb-30">
+                    <div class="product-box bird-toys">
+                        <div class="card" style="width: 18rem;">
+                            <img src="assets/flappy bird.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Flappy bored</h5>
+                                <p class="card-text">Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff.</p>
+                                <p>€ <span>7.90</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mt-40 mb-30">
+                    <div class="product-box fish-toys">
+                        <div class="card" style="width: 18rem;">
+                            <img src="assets/fish toy.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">It's-a-rock</h5>
+                                <p class="card-text">Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff. Please buy my stuff.</p>
+                                <p>€ <span>33.60</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                </div>
+                <?php foreach ($products as $i => $product): ?>
+                    <label>
+                        <?php // <?p= is equal to <?php echo ?>
+                        <input type="checkbox" value="1" name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?> -
+                        &euro; <?= number_format($product['price'], 2) ?>
+                    </label><br />
+                <?php endforeach; ?>
+            </fieldset>
+
+            <button type="submit" class="btn btn-primary">Order!</button>
+        </form>
+
+        <footer>You already ordered <strong>&euro; <?php echo $totalValue ?></strong> in toys for your pet(s).</footer>
+    </div>
+
+<style>
+    footer {
+        text-align: center;
+    }
+</style>
+</body>
+</html>
+
+<?php
+
+function pre_r( $array ) {
+    echo '<pre>';
+    print_r($array);
+    echo '</pre>';
+}
+
+?>
